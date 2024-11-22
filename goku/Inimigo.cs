212b78e0a00541c52ef4460830b23d1442aff64e
Matheus <matheusvenanciodc@gmail.com>
2024-11-22 10:54:57 -0300

    colocar as image

--- goku/Inimigo.cs
+++ goku/Inimigo.cs
@@ -1,17 +1,23 @@
 using FFImageLoading.Maui;
-namespace Goku
+namespace Goku;
 
 public class Inimigo
-{ 
-
-
-
-
-
-
-
-
-
-
-
+{
+    image imageView;
+    public Inimigo (image a)
+    {
+        imageView = a;
+    }
+    public void MoveX(double s)
+    {
+        imageView.TranslationX-=s;
+    }
+    public double GetX()
+    {
+        return imageView.TranslationX;
+    }
+    public void Reset()
+    {
+        imageView.TranslationX=500;
+    }
 }
--- goku/Inimigos.cs
+++ goku/Inimigos.cs
@@ -1,18 +1,43 @@
 using FFImageLoading.Maui;
+
 namespace Goku
-
-public class Inimigos
 {
-    
+    public class Inimigos
+    {
+        // Lista para armazenar todos os inimigos
+        private List<Inimigo> inimigos = new List<Inimigo>();
 
+        // Referência para o inimigo atual (se houver)
+        private Inimigo atual = null;
 
+        // Coordenada mínima X (para posicionamento)
+        private double minX;
 
+        // Construtor para inicializar com o valor de minX
+        public Inimigos(double a)
+        {
+            minX = a;
+        }
 
+        // Adiciona um inimigo à lista
+        public void Add(Inimigo inimigo)
+        {
+            inimigos.Add(inimigo);
+            if (atual == null)
+            {
+                // Define o primeiro inimigo adicionado como o atual
+                atual = inimigo;
+            }
+            Iniciar(); // Inicializa ou reseta os inimigos
+        }
 
-    
-
-
-
-
-
+        // Reseta todos os inimigos
+        public void Iniciar()
+        {
+            foreach (var inimigo in inimigos)
+            {
+                inimigo.Reset(); // Presume-se que o método Reset existe na classe Inimigo
+            }
+        }
+    }
 }
